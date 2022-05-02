d3d5413a10edb4ad4490d01d6971d4164eed3f93
roman <romankrasawa8567@gmail.com>
2022-05-02 22:26:00 +0300

    error fix project_4

--- project_4/project_4.py
+++ project_4/project_4.py
@@ -8,17 +8,20 @@ ui.setupUi(MainWindow)
 MainWindow.show()
 
 def convert():
-    oldNum = ui.oldNum.text()
-    oldType = int(ui.oldType.text())
-    newType = int(ui.newType.text())
-    if newType == 2:
-        ui.newNum.setText(str(bin(int(oldNum, oldType))))
-    elif newType == 8:
-        ui.newNum.setText(str(oct(int(oldNum, oldType))))
-    elif newType == 10:
-        ui.newNum.setText(str(int(oldNum, oldType)))
-    elif newType == 16:
-        ui.newNum.setText(str(hex(int(oldNum, oldType))))
+    try:
+        oldNum = ui.oldNum.text()
+        oldType = int(ui.oldType.text())
+        newType = int(ui.newType.text())
+        if newType == 2:
+            ui.newNum.setText(str(bin(int(oldNum, oldType))))
+        elif newType == 8:
+            ui.newNum.setText(str(oct(int(oldNum, oldType))))
+        elif newType == 10:
+            ui.newNum.setText(str(int(oldNum, oldType)))
+        elif newType == 16:
+            ui.newNum.setText(str(hex(int(oldNum, oldType))))
+    except:
+        pass
 ui.convert.clicked.connect(convert)
 
 sys.exit(app.exec_())
