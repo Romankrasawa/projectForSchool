d1d6695974d0d8d14e4432ff4d3b06dda88ccc12
roman <romankrasawa8567@gmail.com>
2022-05-03 09:47:42 +0300

    gui_3 робоча версія без сss

--- project_3/project_3.py
+++ project_3/project_3.py
@@ -13,11 +13,13 @@ alphabet = "абвгґдеєжзиіїйклмнопрстуфхцчшщьюя"
 field = []
 cord = [0,0]
 off_game =True
+moves = 0
 
 def generateField():
-    global off_game,field,size,cord
+    global off_game,field,size,cord, moves
     try:
-        if off_game:
+        moves = ui.moves.value()
+        if off_game and moves>0:
             size = int(ui.size.text())
             if size > 1:
                 field = [[alphabet[random.randint(0, len(alphabet) - 1)] for i in range(size)] for i in range(size)]
@@ -31,40 +33,60 @@ def generateField():
                 ui.field.setText(text[:-1])
                 cord = [0, 0]
                 off_game = False
-        else:
+        elif not(off_game):
             msg = QMessageBox()
             msg.setWindowTitle("Повідомлення")
             msg.setText("Щоб почати нову гру вам потрібно завершити цю")
+            msg.exec_()
+        else:
+            msg = QMessageBox()
+            msg.setWindowTitle("Повідомлення")
+            msg.setText("Щоб почати гру вам потрібно вибрати приємлиму кількість ходів")
             msg.exec_()
     except:
         pass
 
 def move():
-    global cord,size,field,off_game
+    global cord,size,field,off_game,moves
     try:
-        letter = ui.letter.text()
-        nearCord = [[cord[0], cord[1] + 1], [cord[0], cord[1] - 1], [cord[0] + 1, cord[1] + 1],[cord[0] - 1, cord[1] + 1], [cord[0] - 1, cord[1] - 1], [cord[0] + 1, cord[1] - 1],[cord[0] + 1, cord[1]], [cord[0] - 1, cord[1]]]
-        for i in range(8):
-            try:
-                if field[nearCord[i][0]][nearCord[i][1]] == letter:
-                    field[nearCord[i][0]][nearCord[i][1]], field[cord[0]][cord[1]] = field[cord[0]][cord[1]],field[nearCord[i][0]][nearCord[i][1]]
-                    cord = nearCord[i]
-                    ui.letter.clear()
-                    break
-            except:
-                pass
-        text = ""
-        for i in range(size):
-            text += "    ".join(field[i]) + "\n"
-        ui.field.setText(text[:-1])
-        if cord == [size - 1, size - 1]:
+        if moves == 0:
             msg = QMessageBox()
-            msg.setWindowTitle("Congratulations")
-            msg.setText("Мої вітання ви пройшли тепер ви можете почати заново")
+            msg.setWindowTitle("Ви програли")
+            msg.setText("Ваші ходи вичерпані спробуйте заново заново")
             msg.exec_()
             cord = [0, 0]
             ui.field.clear()
+            ui.letter.clear()
             off_game = True
+        else:
+            letter = ui.letter.text()
+            ui.letter.clear()
+            if not(letter in "qwertyuiopasdfghjklzxcvbnm,./;'[]1234567890-=/*+?><:!") and len(letter) == 1:
+                moves -= 1
+            nearCord = [[cord[0], cord[1] + 1], [cord[0], cord[1] - 1], [cord[0] + 1, cord[1] + 1],[cord[0] - 1, cord[1] + 1], [cord[0] - 1, cord[1] - 1], [cord[0] + 1, cord[1] - 1],[cord[0] + 1, cord[1]], [cord[0] - 1, cord[1]]]
+            for i in range(8):
+                try:
+                    if field[nearCord[i][0]][nearCord[i][1]] == letter:
+                        field[nearCord[i][0]][nearCord[i][1]], field[cord[0]][cord[1]] = field[cord[0]][cord[1]],field[nearCord[i][0]][nearCord[i][1]]
+                        cord = nearCord[i]
+                        break
+                except:
+                    pass
+            text = ""
+            for i in range(size):
+                text += "    ".join(field[i]) + "\n"
+            ui.field.setText(text[:-1])
+            ui.moves.setValue(moves)
+            if cord == [size - 1, size - 1]:
+                msg = QMessageBox()
+                msg.setWindowTitle("Congratulations")
+                msg.setText("Мої вітання ви пройшли тепер ви можете почати заново")
+                msg.exec_()
+                cord = [0, 0]
+                ui.moves.setValue(0)
+                ui.field.clear()
+                ui.letter.clear()
+                off_game = True
     except:
         pass
 ui.generateField.clicked.connect(generateField)
